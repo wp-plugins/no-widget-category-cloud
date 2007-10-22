8f6124e961c63588f8171caa8c78a270e6df3aa5
sivel <sivel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-10-22 12:42:39 +0000

    Committing initial public release
    git-svn-id: https://plugins.svn.wordpress.org/no-widget-category-cloud/trunk@22841 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ no-widget-category-cloud.php
@@ -0,0 +1,95 @@
+<?php
+/*
+Plugin Name: No Widget Category Cloud
+Plugin URI: http://sivel.net/archives/category/wordpress/plugins/
+Description: Creates a function that can be placed in a wordpress template for a category cloud that exists without the requirement of widgets or a widget ready theme.
+Version: 0.2
+Author:  Matt Martz <mdmartz@sivel.net>
+Author URI: http://sivel.net
+
+        Copyright (c) 2007 Matt Martz (http://sivel.net)
+        No Widget Category Cloud is released under the GNU General Public License (GPL)
+        http://www.gnu.org/licenses/gpl.txt
+*/
+
+function nw_catcloud($small=75,$big=200,$unit='%',$align='left',$orderby='name',$order='ASC',$min=1,$hide_empty=1,$title='',$exclude='') {
+
+	$options = array('small' => '', 'big' => '', 'unit' => '', 'align' => '', 'orderby' => '', 'order' => '', 'min' => '', 'hide_empty' => '', 'title' => '', 'exclude' => '');
+
+	foreach ( $options as $key => $value )
+                        $options[$key] = $$key;
+
+	// omit title if not specified
+	if ($options['title'] != '')
+		echo $before_title . $options['title'] . $after_title;
+
+	if ($options['exclude'] != '')
+		$exclude = '&exclude=' . $options['exclude'];
+
+	$hide_empty = '&hide_empty=' . $options['hide_empty'];
+
+	// check which version of wp is being used
+	if ( function_exists('get_categories') ) {
+		// new version of wp (2.1+)
+		$cats = get_categories("style=cloud&show_count=1&use_desc_for_title=0$exclude&hierarchical=0$hide_empty");
+
+		foreach ($cats as $cat) {
+			$catlink = get_category_link( $cat->cat_ID );
+			$catname = $cat->cat_name;
+			$count = $cat->category_count;
+			if ($count >= $options['min']) {
+				$counts{$catname} = $count;
+				$catlinks{$catname} = $catlink;
+			}
+		}
+
+	} else	{
+		// old version of wp (pre-2.1)
+		$cats = wp_list_cats("list=0&sort_column=name&optioncount=1&use_desc_for_title=0$exclude&recurse=1&hierarchical=0$hide_empty");
+
+		$cats = explode("<br />\n", $cats);
+		foreach ($cats as $cat) {
+			$regs = array(); // initialise the regs array
+			eregi("a href=\"(.+)\" ", $cat, $regs);
+			$catlink = $regs[1];
+			$cat = trim(strip_tags($cat));
+			eregi("(.*) \(([0-9]+)\)$", $cat, $regs);
+			$catname = $regs[1];
+			$count = $regs[2];
+			if ($count >= $options['min']) {
+				$counts{$catname} = $count;
+				$catlinks{$catname} = $catlink;
+			}
+		}
+	}
+
+	$spread = max($counts) - min($counts);
+	if ($spread <= 0) { $spread = 1; };
+	$fontspread = $options['big'] - $options['small'];
+	$fontstep = $spread / $fontspread;
+	if ($fontspread <= 0) { $fontspread = 1; }
+
+	echo '<p class="catcloud" style="text-align:'.$options['align'].';">';
+
+	if ('count' == $options['orderby']) {
+		if ('DESC' == $options['order'])
+			arsort($counts);
+		else
+			asort($counts);
+	} elseif ('name' == $options['orderby']) {
+		if ('DESC' == $options['order'])
+			uksort($counts, create_function('$a, $b', 'return -(strnatcasecmp($a, $b));'));
+		else
+			uksort($counts, 'strnatcasecmp');
+	}
+
+	foreach ($counts as $catname => $count) {
+		$catlink = $catlinks{$catname};
+		echo "\n<a href=\"$catlink\" title=\"$count posts filed under $catname\" style=\"line-height:1.2; font-size:".
+			($options['small'] + ceil($count/$fontstep)).$options['unit']."\">$catname</a> ";
+	}
+
+	echo '</p>';
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,79 @@
+=== Plugin Name ===
+Contributors: sivel
+Tags: formatting, links, post, posts, categories, cloud, tags
+Requires at least: 2.0
+Tested up to: 2.3
+Stable tag: 0.2
+
+Creates a function that can be placed in a wordpress template for a category cloud that exists without the requirement of widgets or a widget ready theme.
+
+== Description ==
+
+Creates a function that can be placed in a wordpress template for a category cloud that exists without the requirement of widgets or a widget ready theme.
+
+I created this plugin after I started using a single column theme that is not widget ready.  I found that there were multiple plugin widgets available, but no plugin that would allow me to just place a php function into my theme to display a category cloud.
+
+Some ideas for oter uses of this plugin:
+
+1. Install WP-Sticky and Exec-PHP.  Create a Sticky post which will stay at the top of your page and type the php code for this plugin in the post.  Now you have a Category Cloud that stays at the top of your page.
+
+== Installation ==
+
+1. Upload the `no-widget-category-cloud` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+NOTE: See "Other Notes" for Upgrade and Usage Instructions as well as other pertinent topics.
+
+== Screenshots ==
+
+1. Screenshot of end product.
+
+== Requirements ==
+
+1. Wordpress 2.x
+1. Web server that supports PHP
+
+== Upgrade ==
+
+1. Deactivate the plugin through the 'Plugins' menu in WordPress
+1. Delete the previous `no-widget-category-cloud` folder from the `/wp-content/plugins/` directory
+1. Upload the new `no-widget-category-cloud` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Usage ==
+
+`<?php nw_catcloud(small_size,big_size,size_unit,align,orderby,order,min_posts,hide_empty,title); ?>`
+
+`small_size = font size, integer (default 75)
+big_size = font size, integer (default 200)
+size_unit = %, px, pt (default %)
+align = left, right, center, justify (default left)
+orderby = count, name (default name)
+order = asc, desc (default asc)
+min_posts = minimum number of posts, integer (default 1)
+hide_empty = 0,1 (default 1, 1=yes,0=no)
+title = string (This can contain HTML to format the title)` 
+
+1. Open the theme files, in your favorite editor, that you wish to add the category cloud to (index.php, single.php, page.php, etc...).
+1. Add a line that looks like above.  You can also use the defaults by not specifiying anything between the parentheses. See example 2 below.
+1. Enjoy.
+1. As I mentioned in the description you can also use this plugin with Exec-PHP and it would make a nice combo with WP-Sticky.
+
+= Examples =
+
+1. <?php nw_catcloud(75,200,'%','left','name','asc',1,1,'<h2 class="posttitle" style="margin-bottom:0px;">Categories</h2>'); ?>
+1. <?php nw_catcloud(); ?>
+
+== Change Log ==
+
+= 0.2 (2007-10-22): =
+* Initial Public Release
+
+== To Do ==
+
+1. I am open to suggestions.
+1. I am sure I will think of something.
+
+== Credit ==
+
+I've got to give credit where credit is due.  And that credit goes to Lee Kelleher and his Category Cloud Widget.  I used a lot of his code in this plugin modifying it where needed to make it work the way I wanted.
